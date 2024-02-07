b182b46a4e8ce16482bd21a76b883645f932d711
bragelbytes <joebragel@gmail.com>
2024-02-06 18:28:40 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto", from: "3.2.0"),
-        .package(url: "https://github.com/OperatorFoundation/Antiphony", branch: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Antiphony", branch: "1.0.2"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Dice", branch: "release"),
         .package(url: "https://github.com/OperatorFoundation/Gardener", branch: "release"),
