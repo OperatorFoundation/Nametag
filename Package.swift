37fa0b8382896ca79d41154677b6934ed82dfa23
consuelita <adelita@operatorfoundation.org>
2024-02-18 20:46:07 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto", from: "2.1.0"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "3.2.0"),
 
         .package(url: "https://github.com/OperatorFoundation/Antiphony", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
